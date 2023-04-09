677da9ba7e2c78bb097bf9bbcd81c61a2c4526c8
blindingdark <blindingdark@outlook.com>
2023-04-09 22:08:35 +0800

    Show share content.

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register(new URL('/service-worker.js', import.meta.url));
 }
+
+document.addEventListener('alpine:init', () => {
+  const url = new URL(window.location);
+  Alpine.store('shared', url.searchParams.get('text'));
+});
