4a4d6afb8f27306381b65b4b4fe77006b0085fcf
blindingdark <blindingdark@outlook.com>
2023-05-01 23:04:00 +0800

    Basic UI.

--- main.js
+++ main.js
@@ -5,4 +5,24 @@ if ('serviceWorker' in navigator) {
 document.addEventListener('alpine:init', () => {
   const url = new URL(window.location);
   Alpine.store('shared', url.searchParams.get('text'));
+
+  Alpine.data('settings', () => ({
+    showSettings: false,
+    saved: false,
+
+    setAPIKey(key) {
+      localStorage.setItem('openai-apikey', key);
+      this.key = key;
+      this.saved = true;
+
+      _this = this;
+      setTimeout(() => { _this.saved = false; }, 1000);
+    },
+
+    key: localStorage.getItem('openai-apikey'),
+
+    toggle() {
+      this.showSettings = !this.showSettings;
+    }
+  }))
 });
